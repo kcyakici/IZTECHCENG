cb6009fa03885a38c703e12dcd5c1268a76eaa2c
kcyakici <68825960+kcyakici@users.noreply.github.com>
2022-06-14 11:26:51 +0300

    Update get_memory_usage.c

--- CENG322/ceng322hw4/get_memory_usage.c
+++ CENG322/ceng322hw4/get_memory_usage.c
@@ -15,14 +15,14 @@ SYSCALL_DEFINE1(get_memory_usage, int, pid) {
 	unsigned long rss;
 
 	for_each_process(task) { // it works like a for loop for the working processes
-    	if (task_pid_nr(task) == pid) { // task_pid_nr returns the pid of the process
-        	mm = get_task_mm(task); // get the address space of the task, returns NULL if no mm
-        	if (mm) {
-            	rss = get_mm_rss(mm) << PAGE_SHIFT; // get_mm_rss returns the number of pages, so we have to multiply this by the page size (equivalent to shifting with PAGE_SHIFT)
-            	mmput(mm); // to release the mm we have gotten with get_task_mm(), this operation is used
-            	return rss/1024; // the result returns bytes, so I am dividing it to get the same answer with "top" command
-        	}
-    	}
+		if (task_pid_nr(task) == pid) { // task_pid_nr returns the pid of the process
+			mm = get_task_mm(task); // get the address space of the task, returns NULL if no mm
+			if (mm) {
+				rss = get_mm_rss(mm) << PAGE_SHIFT; // get_mm_rss returns the number of pages, so we have to multiply this by the page size (equivalent to shifting with PAGE_SHIFT)
+				mmput(mm); // to release the mm we have gotten with get_task_mm(), this operation is used
+			return rss/1024; // the result returns bytes, so I am dividing it to get the same answer with "top" command
+        		}
+    		}
 	}
 	return -1; // implementing this for a possible error check in future implementations
 }
